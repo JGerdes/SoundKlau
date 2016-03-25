9091fbc1d683aa89721085c7ea8c34a6dd933b92
Jonas Gerdes <jonas.gerdes@gmx.net>
2016-03-25 13:44:32 +0100

    inject download button into site

--- js/klaut.js
+++ js/klaut.js
@@ -1,3 +1,9 @@
 (function(){
-	console.log("Hello SoundCloud!");
+	var buttonContainer = document.querySelector(".sc-button-group.sc-button-group-medium"),
+	downloadButton = document.createElement("button");
+	downloadButton.className = "sc-button sc-button-medium sc-button-responsive sc-button-download sc-button-klau";
+	downloadButton.title = "Download";
+	downloadButton.innerHTML = "Download";
+	buttonContainer.appendChild(downloadButton);
+
 })();
