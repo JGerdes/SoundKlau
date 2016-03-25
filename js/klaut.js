83dd1d31781f8c8ae61d72313f948292093d8030
Jonas Gerdes <jonas.gerdes@gmx.net>
2016-03-25 13:54:20 +0100

    make sure to add button only once

--- js/klaut.js
+++ js/klaut.js
@@ -1,6 +1,15 @@
 (function(){
-	var buttonContainer = document.querySelector(".sc-button-group.sc-button-group-medium"),
-	downloadButton = document.createElement("button");
+	var buttonContainer = document.querySelector(".sc-button-group.sc-button-group-medium");
+	if(buttonContainer === null){
+		return;
+	}
+	var existingButtons = buttonContainer.childNodes;
+	for(var i=0; i<existingButtons.length; i++){
+		if(existingButtons[i].title === "Download"){
+			return;
+		}
+	} 
+	var downloadButton = document.createElement("button");
 	downloadButton.className = "sc-button sc-button-medium sc-button-responsive sc-button-download sc-button-klau";
 	downloadButton.title = "Download";
 	downloadButton.innerHTML = "Download";
